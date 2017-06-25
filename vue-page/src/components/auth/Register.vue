6824fd71bc42a3a87e8576a435082dc067050237
AroniasPrenovost <Aroniascoder@gmail.com>
2017-06-24 18:54:55 -0700

    added routes

---
+++ vue-page/src/components/auth/Register.vue
@@ -0,0 +1,41 @@
+<template>
+   <div>
+       <h3 class='text-center'>Register</h3>
+       
+       <input type='text' class='form-control m-b-15' placeholder='Email Address' v-model='user.email'>
+       <input type='text' class='form-control m-b-15' placeholder='Username' v-model='user.username'>
+       <input type='text' class='form-control m-b-15' placeholder='Password' v-model='user.password'>
+       
+       <hr>
+       
+       <button class='btn btn-lg btn-primary btn-black m-b-15 @click=register'>Register</button>
+       <!-- @click is short for v-on:click -->
+       <p class='text-center'>
+           Already have an account? <router-link to ='/auth/login'>Login!</router-link>
+       </p>
+   </div>
+</template>
+
+<script>
+    export default {
+      name: 'register',
+      data: function () {
+        return {
+          user: {
+            email: '',
+            username: '',
+            password: ''
+          }
+        }
+      },
+      methods: {
+        register: function () {
+          console.log(this.user)
+        }
+      }
+    }
+</script>
+
+<style>
+    
+</style>
--- vue-page/src/main.js
+++ vue-page/src/main.js
@@ -2,12 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-
-Vue.config.productionTip = false
+import Router from './routes.js'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router: Router,
   template: '<App/>',
   components: { App }
 })
